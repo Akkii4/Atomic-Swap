8cacca4d457564e67245d5b42533ea3e3e42df9a
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-09-27 23:40:47 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,14 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const goerliProvider = new HDWalletProvider({
+  privateKeys: ["userA_private_key", "userB_private_key"],
+  providerOrUrl: `https://eth-goerli.alchemyapi.io/v2/YOUR_ALCHEMY_SECRET`,
+});
+
+const binanceProvider = new HDWalletProvider({
+  privateKeys: ["userA_private_key", "userB_private_key"],
+  providerOrUrl: "https://data-seed-prebsc-1-s3.binance.org:8545/",
+});
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -6,7 +17,7 @@
  *
  * More information about configuration can be found at:
  *
- * truffleframework.com/docs/advanced/configuration
+ * trufflesuite.com/docs/advanced/configuration
  *
  * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
  * to sign your transactions before they're sent to a remote public node. Infura accounts
@@ -36,6 +47,16 @@ module.exports = {
    */
 
   networks: {
+    goerli: {
+      provider: () => goerliProvider,
+      network_id: "5",
+      gas: 7000000,
+    },
+    binanceTestnet: {
+      provider: () => binanceProvider,
+      network_id: "97",
+      gas: 5000000,
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -82,7 +103,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.4", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
