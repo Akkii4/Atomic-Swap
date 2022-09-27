f2bdd1bc0fe3a102ff57c06118d76590bce7ddb1
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-09-27 23:40:56 +0530

    Create HashedTimeLocked.sol

---
+++ contracts/HashedTimeLocked.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.7.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract HashedTimeLocked {
+    uint256 public startTime;
+    uint256 public lockTime = 10 minutes;
+    string public secret; //
+    bytes32 public secretHash =
+        0xfabc158879c351adcbfec2bb04a7cbb883f4a555973dac13fe610424639e30f9;
+    address public recipient;
+    address public sender;
+    uint256 public tokenAmount;
+    IERC20 public token;
+
+    constructor(
+        address _recipient,
+        address _token,
+        uint256 _tokenAmount
+    ) {
+        recipient = _recipient;
+        sender = msg.sender;
+        tokenAmount = _tokenAmount;
+        token = IERC20(_token);
+    }
+
+    function deposit() external {
+        startTime = block.timestamp;
+        token.transferFrom(msg.sender, address(this), tokenAmount);
+    }
+
+    function withdraw(string memory _secret) external {
+        require(
+            keccak256(abi.encodePacked(_secret)) == secretHash,
+            "wrong secret"
+        );
+        secret = _secret;
+        token.transfer(recipient, tokenAmount);
+    }
+
+    function refund() external {
+        require(block.timestamp > startTime + lockTime, "too early");
+        token.transfer(sender, tokenAmount);
+    }
+}
